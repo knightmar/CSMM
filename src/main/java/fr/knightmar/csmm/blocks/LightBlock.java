9efec4dc8e84dc3eb2d2aebf138a4ce301f21b77
knightmar <74785484+knightmar@users.noreply.github.com>
2021-03-03 19:42:49 +0100

    changes

--- src/main/java/fr/knightmar/csmm/blocks/LightBlock.java
+++ src/main/java/fr/knightmar/csmm/blocks/LightBlock.java
@@ -1,12 +1,35 @@
 package fr.knightmar.csmm.blocks;
 
+import fr.knightmar.csmm.tileentity.TileEntityLight;
+import fr.knightmar.csmm.tileentity.TileEntityTemp;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
 
 public class LightBlock extends Block {
     public float isLight = 0;
+
+    @Override
+    public boolean hasTileEntity(BlockState state) {
+        return true;
+    }
+
+
+    @Override
+    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
+        return new TileEntityLight();
+    }
 
 
     public LightBlock() {
@@ -14,5 +37,23 @@ public class LightBlock extends Block {
 
     }
 
+    @Override
+    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+
+        if (worldIn.getTileEntity(pos) instanceof TileEntityTemp){
+
+
+
+
+
+
+            TileEntityTemp te = (TileEntityTemp) worldIn.getTileEntity(pos);
+            player.sendStatusMessage(new StringTextComponent("Counter : " + te.getCounter()), true);
+            return ActionResultType.SUCCESS;
+        }
+
+
+        return ActionResultType.PASS;
+    }
 
 }
--- src/main/java/fr/knightmar/csmm/tileentity/TileEntityLight.java
+++ src/main/java/fr/knightmar/csmm/tileentity/TileEntityLight.java
@@ -1,16 +1,46 @@
 package fr.knightmar.csmm.tileentity;
 
-import net.minecraft.tileentity.ITickableTileEntity;
+import fr.knightmar.csmm.init.ModTileEntities;
+import net.minecraft.block.BlockState;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.tileentity.TileEntityType;
 
 public class TileEntityLight extends TileEntity {
-    private boolean isLight = false;
+
+    private int isOn = 0;
 
 
-    public TileEntityLight(TileEntityType<?> tileEntityTypeIn) {
-        super(tileEntityTypeIn);
+
+    public TileEntityLight() {
+        super(ModTileEntities.LIGHT_TILE_ENTITY.get());
+    }
+
+    @Override
+    public void read(BlockState state, CompoundNBT nbt) {
+        super.read(state, nbt);
+
+        this.setState(nbt.getInt("state"));
+    }
+
+    @Override
+    public CompoundNBT write(CompoundNBT compound) {
+        super.write(compound);
+
+        compound.putInt("state", this.getState());
+
+        return compound;
+    }
+
+    public void tick() {
+
     }
 
 
+    public int getState() {
+        return isOn;
+    }
+
+    public void setState(int isOn) {
+        this.isOn = isOn;
+    }
 }
